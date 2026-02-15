0349a22d6b4d7c3026b423336d077d27e7e0b428
Atinction <atinction@gmail.com>
2026-02-15 16:38:40 +0530

    Delete Routes directory

--- Routes/planRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { savePlan } = require("../Controllers/planController");
-
-router.post("/save-plan", savePlan);
-
-module.exports = router;
